c05d8fe81b5ab78a65be79edcb9859faa19300f5
27293 <myltd6@fpt.com.vn>
2020-12-18 14:59:16 +0700

    Initial Commit

---
+++ CustomizeTextStyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomizeTextStyle
+//
+//  Created by Ngo Dang tan on 18/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
